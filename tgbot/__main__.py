77d06025be6d963252da5a85e5fa7d569ce78b6d
tatyana <beatytanya@gmail.com>
2024-11-22 14:47:13 +0500

    Add entry point

--- tgbot/__main__.py
+++ tgbot/__main__.py
@@ -1,70 +1,26 @@
+import os
 import asyncio
-import logging
-
-import betterlogging as bl
-import orjson
 from aiogram import Bot, Dispatcher
-from aiogram.client.bot import DefaultBotProperties
-from aiogram.client.session.aiohttp import AiohttpSession
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
+from handlers.commands import router 
 
-from tgbot import handlers
-from tgbot.data import config
+from dotenv import find_dotenv, load_dotenv 
+load_dotenv(find_dotenv())
+from database.engine import create_db
 
 
-def setup_logging():
-    log_level = logging.INFO
-    bl.basic_colorized_config(level=log_level)
-    logger = logging.getLogger(__name__)
-    logger.info("Starting bot")
 
 
-def setup_handlers(dp: Dispatcher) -> None:
-    dp.include_router(handlers.setup())
-
-
-def setup_middlewares(dp: Dispatcher) -> None:
-    pass
-
-
-async def setup_aiogram(dp: Dispatcher) -> None:
-    setup_handlers(dp)
-    setup_middlewares(dp)
-
-
-async def aiogram_on_startup_polling(dispatcher: Dispatcher, bot: Bot) -> None:
-    await setup_aiogram(dispatcher)
-
-
-async def aiogram_on_shutdown_polling(dispatcher: Dispatcher, bot: Bot) -> None:
-    await bot.session.close()
-    await dispatcher.storage.close()
 
 
 async def main():
-    setup_logging()
-    session = AiohttpSession(
-        json_loads=orjson.loads,
-    )
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
+    dp = Dispatcher()    
+    dp.include_routers(router)
 
-    bot = Bot(
-        token=config.BOT_TOKEN,
-        session=session,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-    )
-
-    storage = MemoryStorage()
-
-    dp = Dispatcher(
-        storage=storage,
-    )
-
-    dp.startup.register(aiogram_on_startup_polling)
-    dp.shutdown.register(aiogram_on_shutdown_polling)
-
-    await dp.start_polling(bot)
+    await create_db()
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())  
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     asyncio.run(main())
